f318df5ff36d08328f268de81266a06f2a82f165
Rahma Alawi Hussein <rahmahusseintz@gmail.com>
2023-11-04 13:45:40 +0300

    added new posts and filter querries from django templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def post_list(request):
-    return render(request, "blog/post_list.html", {})
+    posts = Post.objects.order_by("published_date")
+
+    return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def post_view(request):
+    return render(request, "blog/post_view.html", {})
