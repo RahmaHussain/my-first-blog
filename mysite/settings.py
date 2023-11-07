ee171731bd5a40478299b579d09f64a78aeb57c6
Rahma Alawi Hussein <rahmahusseintz@gmail.com>
2023-11-07 21:12:20 +0300

    Modified Security

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,9 +47,9 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
